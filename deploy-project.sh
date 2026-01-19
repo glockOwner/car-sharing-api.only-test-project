2a3359fefe968daa5807a279bece9982ac989fa8
glockOwner <danil.avdeev.2004@gmail.com>
2026-01-19 15:30:16 +0400

    deploy file was updated

--- deploy-project.sh
+++ deploy-project.sh
@@ -1,1 +1,1 @@
-cp .env.example .env && source ./.env && ./vendor/bin/sail up -d && docker exec -i car-sharing-api-mysql_db-1 mysql -usail -ppassword laravel < ./backup.sql
+composer install && cp .env.example .env && source ./.env && ./vendor/bin/sail up -d && docker exec -i car-sharing-api-mysql_db-1 mysql -usail -ppassword laravel < ./backup.sql
